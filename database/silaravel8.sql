139e8c64907de4fbf81587044ac7d06586b9fe1c
syaifuddin246 <syaifuddin@gmail.com>
2022-04-26 08:31:50 +0700

    database

--- database/silaravel8.sql
+++ database/silaravel8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2022 at 09:56 AM
+-- Generation Time: Apr 26, 2022 at 02:21 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
